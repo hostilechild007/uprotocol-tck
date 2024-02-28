716a4768dadc65106c91820b21488cf64f731ff5
CZ0GM3 <divya.vuppala@gm.com>
2024-02-28 14:41:46 -0500

    Reformatting the test_socket_ta.py

--- python/examples/tck_interoperability/test_socket_ta.py
+++ python/examples/tck_interoperability/test_socket_ta.py
@@ -46,7 +46,9 @@ from uprotocol.proto.uattributes_pb2 import UPriority
 from uprotocol.transport.builder.uattributesbuilder import UAttributesBuilder
 from uprotocol.uri.serializer.longuriserializer import LongUriSerializer
 
-from up_client_socket_python.utils.socket_message_processing_utils import convert_json_to_jsonstring, convert_str_to_bytes, protobuf_to_base64, send_socket_data
+from up_client_socket_python.utils.socket_message_processing_utils import convert_json_to_jsonstring, convert_str_to_bytes, \
+    protobuf_to_base64, send_socket_data
+
 
 class SocketUListener(UListener):
     def __init__(self, test_manager_conn: socket.socket) -> None:
@@ -71,33 +73,36 @@ class SocketUListener(UListener):
 
         if topic is not None:
             attributes.source.CopyFrom(topic)
-        
+
         umsg: UMessage = UMessage(attributes=attributes, payload=payload)
-        
+
         json_message = {
             "action": "onReceive",
-            "message": protobuf_to_base64(umsg) 
+            "message": protobuf_to_base64(umsg)
         }
 
-        json_message_str: str = convert_json_to_jsonstring(json_message) 
+        json_message_str: str = convert_json_to_jsonstring(json_message)
 
-        message: bytes = convert_str_to_bytes(json_message_str) 
+        message: bytes = convert_str_to_bytes(json_message_str)
 
         print("Sending onReceive to Test Manager Directly!")
-        send_socket_data(self.test_manager_conn, message) 
+        send_socket_data(self.test_manager_conn, message)
 
-        return UStatus(code=UCode.OK, message="all good") 
-    
+        return UStatus(code=UCode.OK, message="all good")
+
 
 uri: str = "/body.access//door.front_left#Door"
 
+
 def build_cloud_event():
     return CloudEvent(spec_version="1.0", source="https://example.com", id="HARTLEY IS THE BEST")
+
 
 def build_upayload():
     any_obj = Any()
     any_obj.Pack(build_cloud_event())
     return UPayload(format=UPayloadFormat.UPAYLOAD_FORMAT_PROTOBUF, value=any_obj.SerializeToString())
+
 
 def build_uattributes():
     return UAttributesBuilder.publish(UPriority.UPRIORITY_CS4).build()
@@ -109,20 +114,18 @@ if __name__ == "__main__":
 
     transport = TransportLayer()
     transport.set_socket_config(dispatcher_IP, dispatcher_PORT)
-    
+
     test_manager_IP: str = "127.0.0.5"
     test_manager_PORT: int = 12345
     test_agent_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    test_agent_socket.connect((test_manager_IP, test_manager_PORT))  
+    test_agent_socket.connect((test_manager_IP, test_manager_PORT))
 
     listener: UListener = SocketUListener(test_agent_socket)
 
     agent = SocketTestAgent(test_agent_socket, transport, listener)
 
-
     topic = LongUriSerializer().deserialize(uri)
     payload: UPayload = build_upayload()
     attributes: UAttributes = build_uattributes()
-
 
     agent.send_to_TM({'SDK_name': "python"})
