5881f1202bc510c974f31d21514b4f846e5108f7
EeshaKotla <eeshareddy199@gmail.com>
2024-03-07 17:08:11 -0500

    serialize feature file changes and implementation

--- BDD/features/steps/tck_step_implementations.py
+++ BDD/features/steps/tck_step_implementations.py
@@ -29,11 +29,12 @@ from behave.runner import Context
 from hamcrest import assert_that, equal_to
 
 from uprotocol.proto.upayload_pb2 import UPayload
-from uprotocol.proto.uri_pb2 import UResource  
+from uprotocol.proto.uri_pb2 import UResource
 from uprotocol.proto.ustatus_pb2 import UStatus
 from uprotocol.proto.ustatus_pb2 import UCode
 from uprotocol.proto.uri_pb2 import UUri
-from uprotocol.proto.uattributes_pb2 import UAttributes, UPriority, UMessageType
+from uprotocol.proto.uattributes_pb2 import UAttributes, UPriority, \
+    UMessageType
 from uprotocol.proto.umessage_pb2 import UMessage
 from uprotocol.uri.serializer.longuriserializer import LongUriSerializer
 from uprotocol.uri.serializer.microuriserializer import MicroUriSerializer
@@ -47,16 +48,22 @@ from protobuf_builders.umessagebuilder import UMessageBuilder
 from protobuf_builders.upayloadbuilder import UPayloadBuilder
 from uprotocol.transport.builder.uattributesbuilder import UAttributesBuilder
 
-from up_client_socket_python.utils.constants import SEND_COMMAND, REGISTER_LISTENER_COMMAND, UNREGISTER_LISTENER_COMMAND, INVOKE_METHOD_COMMAND, LONG_URI_SERIALIZE, LONG_URI_DESERIALIZE, MICRO_URI_SERIALIZE, MICRO_URI_DESERIALIZE
+from up_client_socket_python.utils.constants import SEND_COMMAND, \
+    REGISTER_LISTENER_COMMAND, UNREGISTER_LISTENER_COMMAND, \
+    INVOKE_METHOD_COMMAND, LONG_URI_SERIALIZE, LONG_URI_DESERIALIZE, \
+    MICRO_URI_SERIALIZE, MICRO_URI_DESERIALIZE
 
 from getters.umessagegetter import UMessageGetter
 from getters.upayloadgetter import UPayloadGetter
+
 
 @given(u'“{sdk_name}” creates data for "{command}"')
 @when(u'“{sdk_name}” creates data for "{command}"')
 def step_impl(context, sdk_name: str, command: str):
     context.logger.info("Inside create register listener data")
     context.json_array = {}
+    if command == "longuriserialize" or command == "microuriserialize":
+        context.serialized_data = ''
 
     while not context.tm.has_sdk_connection(sdk_name):
         continue
@@ -79,17 +86,53 @@ def step_impl(context: Context, key: str, value: str):
     Can i prep data w/ data type already when putting value in dict?
     
     '''
-    context.logger.info("Json data: Key is " + str(key) + " value is " + str(value))
+    context.logger.info(
+        "Json data: Key is " + str(key) + " value is " + str(value))
     if key not in context.json_array:
-        context.json_array[key] = [value]
+        if key == "serializeddata":
+            context.json_array[key] = [context.serialized_data]
+        else:
+            context.json_array[key] = [value]
 
 
 @given(u'sends "{command}" request')
 @when(u'sends "{command}" request')
 def step_impl(context, command: str):
-    context.logger.info(f"Json request for {command} -> {str(context.json_array)}")
-    context.status = context.tm.json_action_request(context.json_array)
-    context.logger.info(f"Status Received: {context.status}")
+    context.logger.info(
+        f"Json request for {command} -> {str(context.json_array)}")
+    if command == "longuriserialize" or command == "microuriserialize":
+        context.serialized_data = context.tm.json_action_request(
+            context.json_array)
+        context.logger.info(
+            f"Serialized data received: {context.serialized_data}")
+    else:
+        context.status = context.tm.json_action_request(context.json_array)
+        context.logger.info(f"Status Received: {context.status}")
+
+
+@when(u'“{ue}” sends request to "{command}" using the serialized data')
+def step_impl(context, ue, command):
+    context.json_array = {}
+    context.json_array['ue'] = [ue]
+    context.json_array['action'] = [command]
+
+    if context.serialized_data not in ['', None]:
+        context.deserialized_data = context.tm.json_action_request(
+            context.json_array)
+        context.logger.info(
+            f"Deserialized data received: {context.deserialized_data}")
+    else:
+        raise ValueError(
+            "Serialized Data is empty or Null. Cannot send deserialize request")
+
+
+@then(u'deserialized data from “{ue2}” is equal to data created by “{ue1}”')
+def step_impl(context, ue2, ue1):
+    if context.deserialized_data not in ['', None]:
+        context.logger.info(f"Deserialized Data: {context.deserialized_data}")
+        #TODO:Validation for received deserialized data to be added.
+    else:
+        raise ValueError("DeSerialized Data is empty or Null")
 
 
 @step(u'the status for "{command}" request is "{status}"')
@@ -103,8 +146,10 @@ def step_impl(context, sdk_name, key, value):
     try:
         if context.tm.received_umessage not in ['', None]:
             received_payload: UPayload = context.tm.received_umessage.payload
-            context.logger.info(f"Payload data for {sdk_name} is {received_payload}")
-            assert_that(received_payload.value.decode('utf-8'), equal_to(value))
+            context.logger.info(
+                f"Payload data for {sdk_name} is {received_payload}")
+            assert_that(received_payload.value.decode('utf-8'),
+                        equal_to(value))
         else:
             raise ValueError(f"Received empty payload for {sdk_name}")
 
@@ -112,181 +157,229 @@ def step_impl(context, sdk_name, key, value):
         raise AssertionError(f"Assertion error. Expected is {value} but "
                              f"received {received_payload.value.decode('utf-8')}",
                              exc_info=ae)
-        
+
+
+@step(u'"{sdk_name}" receives serialized data')
+def step_impl(context, sdk_name):
+    try:
+        if context.serialized_data not in ['', None]:
+            context.logger.info(
+                f"Received serialized Data as {context.serialized_data}")
+            assert_that(context.serialized_data, not (equal_to('')))
+        else:
+            raise ValueError(f"Received empty data for {sdk_name}")
+
+    except AssertionError as ae:
+        raise AssertionError("Assertion error. Expected some serialied data "
+                             f"but received {context.serialized_data}: {ae}")
+    except Exception as ex:
+        context.logger.error(f"Exception occurs: {ex}")
+        raise
+
+
 @given('"{sdk_name}" is connected to the Test Manager')
 def tm_connects_to_ta_socket(context, sdk_name: str):
     test_manager: SocketTestManager = context.tm
-    
+
     start_time: float = time.time()
     end_time: float = start_time
     wait_sec: float = 7.0
-    
+
     while not test_manager.has_sdk_connection(sdk_name):
         time.sleep(1)
         end_time = time.time()
     if not test_manager.has_sdk_connection(sdk_name):
         context.logger.error(sdk_name + " Test Agent didn't connect in time")
         raise Exception(sdk_name + " Test Agent didn't connect in time")
-    
-    context.logger.info(f"{sdk_name} TA connects to TM {test_manager.sdk_to_test_agent_socket.keys()}")
+
+    context.logger.info(
+        f"{sdk_name} TA connects to TM {test_manager.sdk_to_test_agent_socket.keys()}")
 
 
 @when('"{sdk_name}" closes its client socket connection')
 def tm_closing_ta_socket(context, sdk_name: str):
     test_manager: SocketTestManager = context.tm
     # test_manager.close_ta(sdk_name)
-    context.logger.info(f"TM closed TA connection {sdk_name} {test_manager.sdk_to_test_agent_socket.keys()}")
+    context.logger.info(
+        f"TM closed TA connection {sdk_name} {test_manager.sdk_to_test_agent_socket.keys()}")
     pass
-    
+
+
 @then('Test Manager closes server socket connection to the "{sdk_name}"')
 def step_impl(context, sdk_name: str):
     # context.logger.info("YOOOO SERVER cLOSED")
     pass
 
-@given('protobuf UEntity "{entity}" sets parameter "{param}" equal to string "{value}"')
-def initialize_protobuf(context, entity: str, param: str, value: str):    
+
+@given(
+    'protobuf UEntity "{entity}" sets parameter "{param}" equal to string "{value}"')
+def initialize_protobuf(context, entity: str, param: str, value: str):
     if entity not in context.initialized_data:
         context.initialized_data[entity] = UEntityBuilder()
-    
+
     builder: UEntityBuilder = context.initialized_data[entity]
     builder.set(param, value)
-        
-@given('protobuf UResource "{resrc}" sets parameter "{param}" equal to string "{value}"')
+
+
+@given(
+    'protobuf UResource "{resrc}" sets parameter "{param}" equal to string "{value}"')
 def initialize_protobuf(context, resrc: str, param: str, value: str):
     if resrc not in context.initialized_data:
         context.initialized_data[resrc] = UResourceBuilder()
-    
+
     builder: UResourceBuilder = context.initialized_data[resrc]
     builder.set(param, value)
 
-@given('protobuf UUri "{uuri}" sets parameter "{param}" equal to created protobuf "{value}"')
+
+@given(
+    'protobuf UUri "{uuri}" sets parameter "{param}" equal to created protobuf "{value}"')
 def initialize_protobuf(context, uuri: str, param: str, value: str):
     if uuri not in context.initialized_data:
         context.initialized_data[uuri] = UUriBuilder()
-        
+
         # can later get filled data directly when send request
-        context.initialized_data["uuri_builder"] = context.initialized_data[uuri]
-    
+        context.initialized_data["uuri_builder"] = context.initialized_data[
+            uuri]
+
     builder: UUriBuilder = context.initialized_data[uuri]
     value_builder: Builder = context.initialized_data[value]
-    
+
     builder.set(param, value_builder.build())
-    
-@given('protobuf UAttributes "{uattr}" creates publish message with parameter source equal to created protobuf "{value}"')
+
+
+@given(
+    'protobuf UAttributes "{uattr}" creates publish message with parameter source equal to created protobuf "{value}"')
 def initialize_protobuf(context, uattr: str, value: str):
     value_builder: Builder = context.initialized_data[value]
-    
-    new_builder: UAttributesBuilder = UAttributesBuilder.publish(value_builder.build(), UPriority.UPRIORITY_CS1)
+
+    new_builder: UAttributesBuilder = UAttributesBuilder.publish(
+        value_builder.build(), UPriority.UPRIORITY_CS1)
     context.initialized_data[uattr] = new_builder
     context.initialized_data["uattributes_builder"] = new_builder
 
-@given('protobuf UPayload "{payload}" sets parameter "{param}" equal to "{value}"')
+
+@given(
+    'protobuf UPayload "{payload}" sets parameter "{param}" equal to "{value}"')
 def initialize_protobuf(context, payload: str, param: str, value: str):
     if payload not in context.initialized_data:
         context.initialized_data[payload] = UPayloadBuilder()
-        
-        context.initialized_data["upayload_builder"] = context.initialized_data[payload]
-    
+
+        context.initialized_data["upayload_builder"] = \
+            context.initialized_data[payload]
+
     builder: UPayloadBuilder = context.initialized_data[payload]
     builder.set(param, value)
 
-    
+
 @when('Test Manager sends "{command}" request to Test Agent "{sdk_name}"')
 def tm_sends_request(context, command: str, sdk_name: str):
     command = command.lower().strip()
-    
+
     if command == REGISTER_LISTENER_COMMAND:
         uuri_builder: UUriBuilder = context.initialized_data["uuri_builder"]
         uuri: UUri = uuri_builder.build()
-        
-        umsg: UMessage =  UMessageBuilder().set_uuri(uuri).build()
-        
-    elif command in [SEND_COMMAND, INVOKE_METHOD_COMMAND]:       
+
+        umsg: UMessage = UMessageBuilder().set_uuri(uuri).build()
+
+    elif command in [SEND_COMMAND, INVOKE_METHOD_COMMAND]:
         uuri_builder: UUriBuilder = context.initialized_data["uuri_builder"]
         uuri: UUri = uuri_builder.build()
-        
-        uattr_builder: UAttributesBuilder = context.initialized_data["uattributes_builder"]
+
+        uattr_builder: UAttributesBuilder = context.initialized_data[
+            "uattributes_builder"]
         uattr: UAttributes = uattr_builder.build()
-        
-        upay_builder: UPayloadBuilder = context.initialized_data["upayload_builder"]
+
+        upay_builder: UPayloadBuilder = context.initialized_data[
+            "upayload_builder"]
         upayload: UPayload = upay_builder.build()
-        
-        umsg: UMessage =  UMessageBuilder().set_uuri(uuri).set_payload(upayload).set_attributes(uattr).build()
-    
-        
+
+        umsg: UMessage = UMessageBuilder().set_uuri(uuri).set_payload(
+            upayload).set_attributes(uattr).build()
+
     # wait till TA is connected
     while not context.tm.has_sdk_connection(sdk_name):
         continue
-    
+
     test_manager: SocketTestManager = context.tm
     context.logger.info(f"sdk_name: {sdk_name}; command: {command}")
     context.status = test_manager.request(sdk_name, command, umsg)
-    
+
     context.logger.info("context.status:")
     context.logger.info(context.status)
 
 
-@when('Test Manager sends "{command}" uri serializer request to Test Agent "{sdk_name}"')
+@when(
+    'Test Manager sends "{command}" uri serializer request to Test Agent "{sdk_name}"')
 def tm_sends_uri_serializer_request(context, command: str, sdk_name: str):
     command = command.lower().strip()
-    
+
     uuri_builder: UUriBuilder = context.initialized_data["uuri_builder"]
     uuri: UUri = uuri_builder.build()
-        
+
     # wait till TA is connected
     while not context.tm.has_sdk_connection(sdk_name):
         continue
-    
+
     test_manager: SocketTestManager = context.tm
-    
+
     if command == LONG_URI_SERIALIZE:
-        translation: str = test_manager.uriserializer_request(sdk_name, command, uuri)
-    
+        translation: str = test_manager.uriserializer_request(sdk_name,
+                                                              command, uuri)
+
     elif command == LONG_URI_DESERIALIZE:
         # Input String and TA should respond with UUri proto
-        
+
         topic_str: str = LongUriSerializer().deserialize(uuri)
-        translation: UUri = test_manager.uriserializer_request(sdk_name, command, topic_str)
-        
+        translation: UUri = test_manager.uriserializer_request(sdk_name,
+                                                               command,
+                                                               topic_str)
+
     context.translation = translation
 
-@then('Test Manager receives an "{status_code}" status for "{command}" request')
+
+@then(
+    'Test Manager receives an "{status_code}" status for "{command}" request')
 def tm_receives_response(context, status_code: str, command: str):
     response_status: UStatus = context.status
     if response_status is None:
-        raise ValueError(f"{command} request did not receive a response UStatus")
+        raise ValueError(
+            f"{command} request did not receive a response UStatus")
 
     status_code = status_code.lower().strip()
     if status_code == "ok":
         assert_that(context.status.code, equal_to(UCode.OK))
         # assert_that(context.status.code, equal_to(UCode.INTERNAL))
-    
-    # reset status var
-    context.status =  None
 
-@then('Test Manager receives OnReceive UMessage from "{sdk_name}" Test Agent with parameter UPayload "{param}" with parameter "{inner_param}" as "{expected}"')
-def tm_receives_onreceive(context, sdk_name: str, param: str, inner_param: str, expected: str):
+    # reset status var
+    context.status = None
+
+
+@then(
+    'Test Manager receives OnReceive UMessage from "{sdk_name}" Test Agent with parameter UPayload "{param}" with parameter "{inner_param}" as "{expected}"')
+def tm_receives_onreceive(context, sdk_name: str, param: str, inner_param: str,
+                          expected: str):
     test_manager: SocketTestManager = context.tm
-    
+
     umsg: UMessage = test_manager.get_onreceive(sdk_name)
-    
+
     umsg_param_getter = UMessageGetter(umsg)
     upay: UPayload = umsg_param_getter.get(param)
-    
+
     actual = UPayloadGetter(upay).get(inner_param)
-        
+
     assert_that(actual.decode('utf-8'), equal_to(expected))
-    
+
+
 @then('Test Manager receives a string "{expected_translation}"')
 def tm_receives_serializer_translation(context, expected_translation: str):
-    
     if context.translation is None:
-        raise ValueError(f"Test Manager did not receive any serializer translation")
+        raise ValueError(
+            f"Test Manager did not receive any serializer translation")
     elif not isinstance(context.translation, str):
         raise ValueError(f"Test Manager did not receive string translation")
 
-    actual_translation: str = context.translation 
+    actual_translation: str = context.translation
     context.translation = None
-    
+
     context.logger.info(f"actual_translation: {actual_translation}")
     assert_that(actual_translation, equal_to(expected_translation))
--- BDD/features/tests/serialize.feature
+++ BDD/features/tests/serialize.feature
@@ -1,28 +1,41 @@
-Scenario Outline: To test the Serializers and send apis
-    Given manager creates data for "<Serializer>" for “<uE1>”
+Feature: Serializer Testing
+Scenario Outline: To test the Long URI Serializers and send apis
+    Given “<uE1>” creates data for "<Serializer>"
       And sets "uri.entity.name" to "body.access"
       And sets "uri.resource.name" to "door"
       And sets "uri.resource.instance" to "front_left"
       And sets "uri.resource.message" to "Door"
-      And sends "<Serializer>" request to "<uE1>"
-      And the status for "<Serializer>" request is "OK"
+      And sends "<Serializer>" request
+      And "<uE1>" receives serialized data
 
-	Then manager receives serialized data from "<uE1>"
-
-	Given manager creates data for "<Serializer>" for "<uE2>"
-		And sets "serializeddata" to "context.serialized_data"
-		and sends "serializeddata" request to "<uE2>"
-		And the status for "<Serializer>" request is "OK"
-	
-	Then manager receives deserialized data from "<uE1>" as "deserializeddatafrombefore"
-	
-
-1. TM -> *sends uuri -> TA (serializes uuri data into a string) -> *replies w/ new string -> TM -> *sends new string -> TA2 (deserizalize it to UURI) -> *sends uuri -> TM
+	When “<uE2>” sends request to "<Deserializer>" using the serialized data
+	Then deserialized data from “<uE2>” is equal to data created by “<uE1>”
 
 Examples:
 
-| uE1       |     Serializer-Deserializer     | uE2     |
-|   Python  |     MicroUri                    | Python  |
-|   Java    |     MicroUri                    | Java    |
-|   Python  |     Long Uri                    | Java    |
-|   Java    |     Long Uri                    | Python  |
+|    uE1    |     Serializer          |    Deserializer     | uE2     |
+|   Python  |     longuriserialize    | longurideserialize  | Python  |
+#|   Python  |     longuriserialize    | longurideserialize  | Java    |
+#|   Java    |     longuriserialize    | longurideserialize  | Python  |
+#|   Java    |     longuriserialize    | longurideserialize  | Java  |
+
+Scenario Outline: To test the Micro URI Serializers and send apis
+
+    Given “<uE1>” creates data for "<Serializer>"
+      And sets "uri.entity.id" to "1"
+      And sets "uri.resource.id" to "2"
+      And sets "uri.authority.id" to "id"
+      And sends "<Serializer>" request
+     And "<uE1>" receives serialized data
+
+    When “<uE2>” sends request to "<Deserializer>" using the serialized data
+
+	Then deserialized data from “<uE2>” is equal to data created by “<uE1>”
+
+Examples:
+
+| uE1       |     Serializer          |     Deserializer     | uE2     |
+|   Python  |     microuriserialize   | microurideserialize  | Python  |
+#|   Java    |     microuriserialize   | microurideserialize  | Java    |
+#|   Python  |     microuriserialize   | microurideserialize  | Java    |
+#|   Java    |     microuriserialize   | microurideserialize  | Python  |
